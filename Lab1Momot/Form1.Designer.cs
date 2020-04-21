272d73b59ff7996de6bd5ed7545dadf1e2d47e76
unknown <yuriypolupan6@gmail.com>
2020-04-21 16:40:46 +0300

    first commit

---
+++ Lab1Momot/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace Lab1Momot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contexToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.menuStrip2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip2.Size = new System.Drawing.Size(910, 28);
+            this.menuStrip2.TabIndex = 5;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.ToolTipText = "Работа с файлом";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(115, 26);
+            this.exitToolStripMenuItem.Text = "Save";
+            this.exitToolStripMenuItem.ToolTipText = "Сохранить данные";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(115, 26);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.ToolTipText = "Выход с программы";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.contexToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.ToolTipText = "Помощь для пользователя";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(135, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.ToolTipText = "Информаци о программе";
+            // 
+            // contexToolStripMenuItem
+            // 
+            this.contexToolStripMenuItem.Name = "contexToolStripMenuItem";
+            this.contexToolStripMenuItem.Size = new System.Drawing.Size(135, 26);
+            this.contexToolStripMenuItem.Text = "Context";
+            this.contexToolStripMenuItem.ToolTipText = "Контекст";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(13, 31);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(897, 772);
+            this.tabControl1.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.tabControl1, "Информация об акциях");
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(889, 743);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Владение акциями";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(847, 388);
+            this.dataGridView1.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.dataGridView1, "Информация о владельцах акции");
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.dataGridView4);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(889, 743);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Акции";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Отобразить";
+            this.toolTip1.SetToolTip(this.button1, "Выберите символ акций и нажмите отобразить для просмотра");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.AllowUserToAddRows = false;
+            this.dataGridView4.AllowUserToDeleteRows = false;
+            this.dataGridView4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView4.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView4.Location = new System.Drawing.Point(4, 381);
+            this.dataGridView4.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.Size = new System.Drawing.Size(882, 348);
+            this.dataGridView4.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView2.Location = new System.Drawing.Point(4, 2);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(882, 299);
+            this.dataGridView2.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.dataGridView2, "Информация об акциях");
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button2);
+            this.tabPage3.Controls.Add(this.dataGridView5);
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(889, 743);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Промышленность";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Отобразить";
+            this.toolTip1.SetToolTip(this.button2, "Выберите код и нажмите отобразить для просмотра");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.AllowUserToAddRows = false;
+            this.dataGridView5.AllowUserToDeleteRows = false;
+            this.dataGridView5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView5.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView5.Location = new System.Drawing.Point(3, 358);
+            this.dataGridView5.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.Size = new System.Drawing.Size(882, 359);
+            this.dataGridView5.TabIndex = 4;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView3.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView3.Location = new System.Drawing.Point(4, 2);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(882, 274);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Информация";
+            // 
+            // helpProvider1
+            // 
+            this.helpProvider1.HelpNamespace = "C:\\Users\\Hruzi\\source\\repos\\Lab1Momot\\Lab1Momot\\momotHelp.chm";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 815);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip2);
+            this.helpProvider1.SetHelpNavigator(this, System.Windows.Forms.HelpNavigator.Topic);
+            this.Name = "Form1";
+            this.helpProvider1.SetShowHelp(this, true);
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contexToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+    }
+}
+
---
+++ Lab1Momot/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1Momot
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Help Label
+           /* this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+
+            // Tell the HelpProvider what controls to provide help for, and
+            // what the help string is.
+            this.helpProvider1.SetShowHelp(this.button1, true);
+            this.helpProvider1.SetHelpString(this.button1, "Select symbol and enter the button 'otobrazit' for see information");
+
+            this.helpProvider1.SetShowHelp(this.button2, true);
+            this.helpProvider1.SetHelpString(this.button2, "Select code and enter the button 'otobrazit' for see information");
+
+            this.helpProvider1.SetShowHelp(this.dataGridView1, true);
+            this.helpProvider1.SetHelpString(this.dataGridView1, "Information about 'Holdings'");
+
+            this.helpProvider1.SetShowHelp(this.dataGridView2, true);
+            this.helpProvider1.SetHelpString(this.dataGridView2, "Information about 'Master'");
+
+            this.helpProvider1.SetShowHelp(this.dataGridView4, true);
+            this.helpProvider1.SetHelpString(this.dataGridView4, "Information about 'Holdings'");
+
+            this.helpProvider1.SetShowHelp(this.dataGridView3, true);
+            this.helpProvider1.SetHelpString(this.dataGridView3, "Information about 'Industry'");
+
+            this.helpProvider1.SetShowHelp(this.dataGridView5, true);
+            this.helpProvider1.SetHelpString(this.dataGridView5, "Information about 'Master'");
+            // Set what the Help file will be for the HelpProvider.
+            this.helpProvider1.HelpNamespace = "mspaint.chm";*/
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Update();
+
+
+        }
+        private void Update()
+        {
+
+            FileDBF dbf = new FileDBF();
+            var dt1 = dbf.Execute(@"SELECT ACCT_NBR AS Номер_Аккаунта, SYMBOL AS Символ, SHARES AS Акции, PUR_PRICE AS Стоимость_Покупки, PUR_DATE AS Дата_покупки FROM holdings.dbf ");
+            var dt2 = dbf.Execute(@"SELECT INDUSTRY AS Индустрия, SYMBOL AS Символ,CO_NAME AS Название_Компании, EXCHANGE AS Биржа, CUR_PRICE AS Цена, YRL_HIGH AS Максимальная_цена, YRL_LOW AS Минимальная_цена, P_E_RATIO AS Соотношение,
+            BETA AS Бета, PROJ_GRTH AS Рост,  PRICE_CHG AS Изменение_цены, SAFETY AS Безопасность, RATING AS Рейтинг, RANK AS Ранк, OUTLOOK AS Прогноз, RCMNDATION AS Рекомендации, RISK AS Риски FROM master.dbf ");
+            var dt3 = dbf.Execute(@"SELECT IND_CODE AS Код, IND_NAME AS Сокращенное_Название, LONG_NAME AS Полное_название  FROM industry.dbf ");
+           
+            dataGridView1.DataSource = dt1;
+            dataGridView2.DataSource = dt2;
+            dataGridView3.DataSource = dt3;
+
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public class FileDBF
+        {
+            private OleDbConnection _connection = null;
+
+            private const string putFileName = @"C:\Users\Hruzi\Desktop\3\3_2Momot3kurs2semestr\Programming Tecnology\lab1\DBDEMOS"; // сюда пишите ПОЛНЫЙ ПУТЬ к ПАПКЕ.
+
+            public DataTable Execute(string command)
+            {
+                DataTable dt = null;
+                if (_connection != null)
+                {
+                    try
+                    {
+                        _connection.Open();
+                        dt = new DataTable();
+                        OleDbCommand oCmd = _connection.CreateCommand();
+                        oCmd.CommandText = command;
+                        dt.Load(oCmd.ExecuteReader());
+                        _connection.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message);
+                    }
+                }
+                return dt;
+            }
+
+            public DataTable GetAll(string dbpath)
+            {
+
+                return Execute("SELECT * FROM " + dbpath); ;
+            }
+
+            public FileDBF()
+            {
+                _connection = new OleDbConnection();
+                _connection.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + putFileName + "; Extended Properties=dBASE IV;";
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView2.SelectedCells[0].ColumnIndex == 1)
+            {
+                FileDBF fld = new FileDBF();
+                string num = dataGridView2.SelectedCells[0].Value.ToString();
+
+                var dt3 = fld.Execute(@"SELECT ACCT_NBR AS Номер_Аккаунта, SYMBOL AS Символ, SHARES AS Акции, PUR_PRICE AS Стоимость_Покупки, PUR_DATE AS Дата_покупки FROM holdings.dbf WHERE SYMBOL = '" + num + "';");
+                dataGridView4.DataSource = dt3;
+
+            }
+
+
+            
+           // var dt1 = dbf.Execute(@"SELECT ACCT_NBR AS Номер_Аккаунта, SYMBOL AS Символ, SHARES AS Акции, PUR_PRICE AS Стоимость_Покупки, PUR_DATE AS Дата_покупки FROM holdings.dbf ");
+           // dataGridView4.DataSource = dt1;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView3.SelectedCells[0].ColumnIndex == 0)
+            {
+                FileDBF fld = new FileDBF();
+                int num = Convert.ToInt32(dataGridView3.SelectedCells[0].Value.ToString());
+                var dt5 = fld.Execute(@"SELECT INDUSTRY AS Индустрия, SYMBOL AS Символ,CO_NAME AS Название_Компании, EXCHANGE AS Биржа, CUR_PRICE AS Цена, YRL_HIGH AS Максимальная_цена, YRL_LOW AS Минимальная_цена, P_E_RATIO AS Соотношение,
+                BETA AS Бета, PROJ_GRTH AS Рост,  PRICE_CHG AS Изменение_цены, SAFETY AS Безопасность, RATING AS Рейтинг, RANK AS Ранк, OUTLOOK AS Прогноз, RCMNDATION AS Рекомендации, RISK AS Риски FROM master.dbf  WHERE INDUSTRY = " + num + ";");
+                dataGridView5.DataSource = dt5;
+            }
+            
+        }
+    }
+}
